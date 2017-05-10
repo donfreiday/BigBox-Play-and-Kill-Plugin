5aac8be57aa4e3768877444875b59817e69b7c2f
don <donfreiday@email.com>
2017-05-09 20:01:30 -0400

    Initial commit.

---
+++ Bigbox Play and Kill/BigBox Play and Kill.cs
@@ -0,0 +1,72 @@
+﻿using Unbroken.LaunchBox.Plugins;
+using Unbroken.LaunchBox.Plugins.Data;
+
+namespace BigBox_Play_and_Kill
+{
+    public class BigBox_Play_and_Kill : IGameMenuItemPlugin
+    {
+        public bool SupportsMultipleGames
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public string Caption
+        {
+            get
+            {
+                return "Play and kill BigBox";
+            }
+        }
+
+        public System.Drawing.Image IconImage
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public bool ShowInLaunchBox
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool ShowInBigBox
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public bool GetIsValidForGame(IGame selectedGame)
+        {
+            return true;
+        }
+
+        public bool GetIsValidForGames(IGame[] selectedGames)
+        {
+            return true;
+        }
+
+        public void OnSelected(IGame selectedGame)
+        {
+            selectedGame.Play();
+            foreach (var process in System.Diagnostics.Process.GetProcessesByName("BigBox"))
+            {
+                process.Kill();
+            }
+        }
+
+        public void OnSelected(IGame[] selectedGames)
+        {
+            return;
+        }
+    }
+}
